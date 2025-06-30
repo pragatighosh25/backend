6c126adad86a1f7eadf69e662b6f5b11582d3292
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-06-30 11:52:27 +0530

    env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
@@ -15,6 +16,6 @@ app.get('/login', (req, res) => {
   res.send('<h1>Please login </h1>')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
