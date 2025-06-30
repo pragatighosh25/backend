428f25ac9e4f2bf6a27bd3714e830af723c44fb1
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-06-30 11:40:01 +0530

    starting a server and making routes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("pragati")
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+  res.send('pragati-id')
+})
+
+app.get('/login', (req, res) => {
+  res.send('<h1>Please login </h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
